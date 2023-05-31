3a2b722e3421ceba6d793701a2a1ffc2aae44f6e
scttgs0 <scttgs0@gmail.com>
2023-05-30 21:43:52 -0500

    update build script to use "Boot from RAM"

--- make_jr.sh
+++ make_jr.sh
@@ -2,7 +2,7 @@
 64tass  --m65c02 \
         --flat \
         --nostart \
-        -o bbombJR.pgx \
+        -o bbombJR.bin \
         --list=bbombJR.lst \
         --labels=bbombJR.lbl \
         boulderbombers.asm
