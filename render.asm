0c3da02a8cc44929c1ff1298b2b64fe5a685ebf4
scttgs0 <scttgs0@gmail.com>
2023-05-30 21:45:02 -0500

    whitespace only.

--- render.asm
+++ render.asm
@@ -6,12 +6,14 @@ ResetCanyon     .proc
                 ldy #0                  ; copy rocks & canyon to screen
 _next1          lda ROCKIMG,Y
                 sta CANYON+40,Y
+
                 iny
                 bne _next1
 
                 ldy #144
 _next2          lda ROCKIMG+255,Y
                 sta CANYON+295,Y
+
                 dey
                 bne _next2
 
