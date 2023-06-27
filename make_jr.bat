dffcb14416e9ed96cf306cb5d32ac10a6c70e550
scttgs0 <scttgs0@gmail.com>
2023-06-26 23:44:26 -0500

    Window build script

---
+++ make_jr.bat
@@ -0,0 +1,8 @@
+
+64tass.exe  --m65c02 ^
+            --flat ^
+            --nostart ^
+            -o bbombJR.bin ^
+            --list=bbombJR.lst ^
+            --labels=bbombJR.lbl ^
+            boulderbombers.asm
